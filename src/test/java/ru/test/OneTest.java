2877c3dc4acddb0456e9798ccb1d699e100ac602
AleksandrovAy <chesterrid1@mail.ru>
2019-03-13 22:26:42 +0300

    oneCommit

---
+++ src/test/java/ru/test/OneTest.java
@@ -0,0 +1,28 @@
+package ru.test;
+
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.*;
+
+public class OneTest {
+
+
+    @Test
+    public void firstTest (){
+        System.out.println("Первый тест");
+    }
+
+
+//    @Test
+//    public void secondTest (){
+//        int a = 5;
+//        int b = 6;
+//        assertThat(a).isEqualTo(b);
+//    }
+
+
+    @Test
+    public void thirdTest (){
+        System.out.println("Третий тест");
+    }
+
+}
